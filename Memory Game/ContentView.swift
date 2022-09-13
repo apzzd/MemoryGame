a5bc5b7209595a152cfa5bb6da8406f0f49bfe21
apzzd <59454160+apzzd@users.noreply.github.com>
2022-09-13 13:10:07 -0600

    Initial Commit

---
+++ Memory Game/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Memory Game
+//
+//  Created by Ada on 9/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Memory Game/Memory_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memory_GameApp.swift
+//  Memory Game
+//
+//  Created by Ada on 9/13/22.
+//
+
+import SwiftUI
+
+@main
+struct Memory_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
